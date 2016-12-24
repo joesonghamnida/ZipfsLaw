a733970a93564b1a7aba27806296b471014d966b
Joe Morris <joe.morris.dev@gmail.com>
2016-12-23 21:31:16 -0600

    created package and files

---
+++ tests/CleanerTests.java
@@ -0,0 +1,5 @@
+/**
+ * Created by joe on 12/23/16.
+ */
+public class CleanerTests {
+}
---
+++ tests/DatabaseTests.java
@@ -0,0 +1,9 @@
+import org.junit.Test;
+
+/**
+ * Created by joe on 12/23/16.
+ */
+
+
+public class DatabaseTests {
+}
