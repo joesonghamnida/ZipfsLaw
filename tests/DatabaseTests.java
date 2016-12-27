6cb244b8adcfff021885197911a24caaa02a5ed3
Joe Morris <joe.morris.dev@gmail.com>
2016-12-26 18:03:00 -0600

    tested load and get all words

--- tests/DatabaseTests.java
+++ tests/DatabaseTests.java
@@ -1,4 +1,11 @@
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.HashMap;
 
 /**
  * Created by joe on 12/23/16.
@@ -6,4 +13,26 @@ import org.junit.Test;
 
 
 public class DatabaseTests {
+
+    //helper method
+    public Connection startConnection()throws SQLException {
+        Connection conn = DriverManager.getConnection("jdbc:h2:mem:test");
+        SQLQueries.createDatabase(conn);
+        return conn;
+    }
+
+    @Test
+    public void loadDataIntoDatabase()throws SQLException{
+        Connection connection = startConnection();
+        HashMap<String, Integer> testData = new HashMap<>();
+        testData.put("alice", 1);
+        testData.put("bob",2);
+        testData.put("charlie",3);
+
+        SQLQueries.loadWordsIntoDB(connection, testData);
+        HashMap<String, Integer> results = SQLQueries.selectAllWords(connection);
+        Assert.assertEquals(results.size(), 3);
+        connection.close();
+    }
+    
 }
