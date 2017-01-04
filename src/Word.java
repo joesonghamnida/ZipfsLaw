352840fc1a01a9dadaf6f07bf7cf840198fe14e9
Joe Morris <joe.morris.dev@gmail.com>
2017-01-03 22:05:33 -0600

    will use for sorting purposes

---
+++ src/Word.java
@@ -0,0 +1,32 @@
+/**
+ * Created by joe on 1/3/17.
+ */
+public class Word {
+
+    private String word;
+    private int frequency;
+
+    public Word() {
+    }
+
+    public Word(String word, int frequency) {
+        this.word = word;
+        this.frequency = frequency;
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public void setWord(String word) {
+        this.word = word;
+    }
+
+    public int getFrequency() {
+        return frequency;
+    }
+
+    public void setFrequency(int frequency) {
+        this.frequency = frequency;
+    }
+}
