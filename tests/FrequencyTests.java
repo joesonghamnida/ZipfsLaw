779fca15655df5953fcf0a9d9094b20fc53a67a1
Joe Morris <joe.morris.dev@gmail.com>
2017-01-15 19:45:10 -0600

    outline

---
+++ tests/FrequencyTests.java
@@ -0,0 +1,21 @@
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+/**
+ * Created by joe on 1/15/17.
+ */
+public class FrequencyTests {
+
+    @Test
+    public void projectedWordFrequencies(){
+        ArrayList<String> test = new ArrayList<>();
+        
+
+    }
+
+    @Test
+    public void actualWordFrequencies(){
+
+    }
+}
