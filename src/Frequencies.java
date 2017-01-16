fad40e851f59d5db06026eceaa114a41f894f3a2
Joe Morris <joe.morris.dev@gmail.com>
2017-01-15 19:45:36 -0600

    outline

---
+++ src/Frequencies.java
@@ -0,0 +1,17 @@
+
+public class Frequencies {
+
+    public static double projectedWordFrequency(){
+
+        double projectedFrequency = 0.0;
+
+        return projectedFrequency;
+    }
+
+    public static double actualWordFrequency(){
+
+        double actualFrequency = 0.0;
+
+        return actualFrequency;
+    }
+}
