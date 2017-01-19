bebc771ed1fa13a1993c010ced8ceb41f3c84182
Joe Morris <joe.morris.dev@gmail.com>
2017-01-19 14:45:12 -0600

    added compare frequencies function

--- src/Frequencies.java
+++ src/Frequencies.java
@@ -3,14 +3,19 @@ import entities.Word;
 import javax.swing.*;
 import java.util.ArrayList;
 
+import static java.lang.System.exit;
+
 public class Frequencies {
+
+    //TODO: need to make projectedWF more flexible in terms of dealing with stuff like a document is one word long,
+    // only contains one word, etc - look at algorithms
 
     public static ArrayList<Double> projectedWordFrequency(ArrayList<String> cleanedText) {
         ArrayList<Double> projectedFrequencies = new ArrayList<>();
 
         double documentSize = cleanedText.size();
 
-        for (int denominator = 2; denominator < 10; denominator++) {
+        for (int denominator = 2; denominator < documentSize; denominator++) {
             double result = documentSize / denominator;
             result = result / documentSize;
             projectedFrequencies.add(result);
@@ -30,7 +35,20 @@ public class Frequencies {
         return actualFrequencies;
     }
 
-    public static void compareFrequencies(ArrayList<Double> actualFrequencies, ArrayList<Double> projectedFrequencies){
+    public static ArrayList<Double> compareFrequencies(ArrayList<Double> actualFrequencies, ArrayList<Double> projectedFrequencies){
 
+        ArrayList<Double> frequencyDifferences = new ArrayList<>();
+
+        //TODO: try / catch
+        if(actualFrequencies.size() != projectedFrequencies.size()){
+            System.out.println("Actual and projected frequency lists differ in size");
+            exit(0);
+        }
+
+        for(int i = 0; i < actualFrequencies.size(); i++){
+            double difference = actualFrequencies.get(i) - projectedFrequencies.get(i);
+        }
+
+        return frequencyDifferences;
     }
 }
