455b9f8cee319cdd9b54eb390da4b88b808c1727
Joe Morris <joe.morris.dev@gmail.com>
2017-01-25 15:51:16 -0600

    refactored text markers

--- src/CleanFile.java
+++ src/CleanFile.java
@@ -1,7 +1,3 @@
-import cleaners.RemoveBlankStrings;
-import cleaners.RemoveNumbers;
-import cleaners.RemovePunctuation;
-import cleaners.RemoveWebsites;
 
 import java.util.ArrayList;
 
@@ -14,15 +10,106 @@ public class CleanFile {
 
         ArrayList<String> cleanedText = new ArrayList<>();
 
-        ArrayList<String> punctuationRemoved = RemovePunctuation.removePunctuation(rawText);
-        ArrayList<String> numbersRemoved = RemoveNumbers.removeNumbers(punctuationRemoved);
-        ArrayList<String> blankStringsRemoved = RemoveBlankStrings.removeBlankStrings(numbersRemoved);
-        ArrayList<String> websitesRemoved = RemoveWebsites.removeWebsites(blankStringsRemoved);
+        ArrayList<String> punctuationRemoved = removePunctuation(rawText);
+        ArrayList<String> numbersRemoved = removeNumbers(punctuationRemoved);
+        ArrayList<String> blankStringsRemoved = removeBlankStrings(numbersRemoved);
+        ArrayList<String> websitesRemoved = removeWebsites(blankStringsRemoved);
 
         //debugCleaning(rawText, punctuationRemoved, numbersRemoved, blankStringsRemoved, websitesRemoved);
 
         cleanedText = websitesRemoved;
         return cleanedText;
+    }
+
+    public static ArrayList<String> removeMarker(ArrayList<String> markedText){
+        ArrayList<String> holdingCell = new ArrayList<>();
+
+        for(int i = 0; i < markedText.size(); i ++){
+            if(!markedText.get(i).contains("REMOVE-ME")){
+                holdingCell.add(markedText.get(i));
+            }
+        }
+
+        markedText = holdingCell;
+
+        return markedText;
+    }
+
+    public static ArrayList<String> removeBlankStrings(ArrayList<String> rawText) {
+        ArrayList<String> blanksRemoved = new ArrayList<>();
+
+        ArrayList<String> holdingCell = new ArrayList<>();
+
+        for (String string : rawText) {
+            if (!(string.equals(""))) {
+                holdingCell.add(string);
+            }
+        }
+
+        blanksRemoved = holdingCell;
+
+        return blanksRemoved;
+    }
+
+    public static ArrayList<String> removeNumbers(ArrayList<String> rawText){
+        ArrayList<String> numbersRemoved = new ArrayList<>();
+
+        String[] numbers = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "0"};
+
+        for(String string : rawText){
+            for(String number : numbers){
+                if(string.contains(number)){
+                    string = string + "REMOVE-ME";
+                }
+            }
+            numbersRemoved.add(string);
+        }
+        numbersRemoved = removeMarker(numbersRemoved);
+        return numbersRemoved;
+    }
+
+
+
+    public static ArrayList<String> removePunctuation(ArrayList<String> rawText) {
+        ArrayList<String> cleanedText = new ArrayList<>();
+        String[] punctuationMarks = {".", ",", ":", ";", "?","<", ">", "\\", "'","{", "}", "*","-","_","(",")","[","]","|", "!", "/", "@", "#", "$", "%", "^", "&", "\"", "+", "~", "`", "="};
+
+        for (String word : rawText) {
+
+            String cleanedWord = "";
+
+            for (String punctuation : punctuationMarks) {
+                if (word.contains(punctuation)) {
+                    cleanedWord = word.replace(punctuation, "");
+                    cleanedWord.trim();
+                    word = cleanedWord;
+                } else {
+                    cleanedWord = word;
+                }
+            }
+            cleanedText.add(cleanedWord.toLowerCase());
+        }
+
+        return cleanedText;
+    }
+
+    public static ArrayList<String> removeWebsites(ArrayList<String> rawText){
+        ArrayList<String> websitesRemoved = new ArrayList<>();
+
+        String[] protocols = {"http", "https", "www"};
+
+        for(String word : rawText){
+            for(String protocol : protocols){
+                if((word.contains(protocol))){
+                    word = word + "REMOVE-ME";
+                }
+            }
+            websitesRemoved.add(word);
+        }
+
+        websitesRemoved = removeMarker(websitesRemoved);
+
+        return websitesRemoved;
     }
 
     public static void debugCleaning(ArrayList<String> rawText, ArrayList<String> punctuationRemoved, ArrayList<String> numbersRemoved, ArrayList<String> blankStringsRemoved, ArrayList<String> websitesRemoved) {
